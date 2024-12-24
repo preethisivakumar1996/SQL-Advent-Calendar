7d12d5cd8d48cc7347fc45b60b30688f06776f0e
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:12:02 +1100

    Update day23_answers.sql
    included qsn

--- MEDIUM/day23_answers.sql
+++ MEDIUM/day23_answers.sql
@@ -1,13 +1,6 @@
+-- The Grinch tracked his weight every day in December to analyze how it changed daily. 
+-- Write a query to return the weight change (in pounds) for each day, calculated as the difference from the previous day's weight.
+
 SELECT day_of_month,
 Weight - LAG(Weight) OVER(Order by (day_of_month)) as Weight_change
 FROM grinch_weight_log;
-
----- OR -----
-
-SELECT 
-    g1.day_of_month,
-    g1.weight - g2.weight AS weight_change
-FROM grinch_weight_log g1
-LEFT JOIN grinch_weight_log g2
-    ON g1.day_of_month = g2.day_of_month + 1
-ORDER BY g1.day_of_month;
