2a203a57bde795ecc3cd0a7bed895f249333660e
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:05:47 +1100

    Update day16_answers.sql
    included qsn

--- MEDIUM/day16_answers.sql
+++ MEDIUM/day16_answers.sql
@@ -1,3 +1,6 @@
+-- As the owner of a candy store, you want to understand which of your products are selling best. 
+-- Write a query to calculate the total revenue generated from each candy category.
+
 SELECT category,
 SUM(quantity_sold*price_per_unit) as Total_Revenue
 FROM candy_sales
