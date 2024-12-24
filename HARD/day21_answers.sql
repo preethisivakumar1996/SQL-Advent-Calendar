29e1bd9c01c7d215b00a9a47272416ac8f6acf73
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:10:05 +1100

    Update day21_answers.sql
    included qsn

--- HARD/day21_answers.sql
+++ HARD/day21_answers.sql
@@ -1,3 +1,8 @@
+-- Santa needs to optimize his sleigh for Christmas deliveries. 
+-- Write a query to calculate the total weight of gifts for each recipient type (good or naughty) and 
+-- determine what percentage of the total weight is allocated to each type. 
+-- Include the recipient_type, total_weight, and weight_percentage in the result.
+
 SELECT 
     recipient_type,
     SUM(weight_kg) AS total_weight,
