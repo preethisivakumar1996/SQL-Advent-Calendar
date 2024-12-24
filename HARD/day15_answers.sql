c84e0f15eb5449bf7fc5d3203e42015d0d89f7a1
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:04:58 +1100

    Update day15_answers.sql
    included qsn

--- HARD/day15_answers.sql
+++ HARD/day15_answers.sql
@@ -1,3 +1,7 @@
+-- A family reunion is being planned, and the organizer wants to identify the three family members with the most children. 
+-- Write a query to calculate the total number of children for each parent and rank them. 
+-- Include the parent’s name and their total number of children in the result.
+
 WITH Rank_Them AS (
     Select family_members.name,
     parent_child_relationships.parent_id, 
