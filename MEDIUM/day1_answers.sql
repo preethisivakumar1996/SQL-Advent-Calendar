85d6f5b6549c3b01e1194c60717132942047c55c
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 15:50:55 +1100

    Update day1_answers.sql
    included qsn

--- MEDIUM/day1_answers.sql
+++ MEDIUM/day1_answers.sql
@@ -1,3 +1,6 @@
+-- A ski resort company want to know which customers rented ski equipment for more than one type of activity (e.g., skiing and snowboarding). 
+-- List the customer names and the number of distinct activities they rented equipment for.
+
 SELECT customer_name, COUNT(DISTINCT activity) AS Distonct_activity_count
 FROM rentals
 GROUP BY customer_name
