7ac436e421c541c8175845731e99267160a99d24
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:08:21 +1100

    Update day19_answers.sql
    included qsn

--- MEDIUM/day19_answers.sql
+++ MEDIUM/day19_answers.sql
@@ -1,3 +1,7 @@
+--Scientists are studying the diets of polar bears. 
+-- Write a query to find the maximum amount of food (in kilograms) consumed by each polar bear in a single meal December 2024. 
+-- Include the bear_name and biggest_meal_kg, and sort the results in descending order of largest meal consumed.
+
 SELECT bear_name, max(food_weight_kg) AS biggest_meal_kg
 FROM polar_bears 
 JOIN meal_log ON polar_bears.bear_id = meal_log.bear_id
