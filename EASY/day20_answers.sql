b2c42869f9a34d57c55ae36ac2fa6f3a54a53a46
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:09:15 +1100

    Update day20_answers.sql
    included qsn

--- EASY/day20_answers.sql
+++ EASY/day20_answers.sql
@@ -1,3 +1,6 @@
+-- We are looking for cheap gifts at the market. 
+-- Which vendors are selling items priced below $10? List the unique (i.e. remove duplicates) vendor names.
+
 SELECT DISTINCT(vendor_name)
 FROM vendors as v
 JOIN item_prices as ip
