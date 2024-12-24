fa22cee6c8b1e2ac99a7d480339dd3a5eca72b2f
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:01:45 +1100

    Update day11_answers.sql
    included qsn

--- EASY/day11_answers.sql
+++ EASY/day11_answers.sql
@@ -1,3 +1,7 @@
+-- You are preparing holiday gifts for your family. 
+-- Who in the family_members table are celebrating their birthdays in December 2024? 
+-- List their name and birthday.
+
 SELECT name, birthday 
 FROM family_members
 WHERE strftime('%m',birthday) = '12' AND strftime('%Y',birthday) = '2024';
