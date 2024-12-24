46b1c8f963b9c31196cea56527a52d0bfb2dd48a
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:10:57 +1100

    Update day22_answers.sql
    included qsn

--- MEDIUM/day22_answers.sql
+++ MEDIUM/day22_answers.sql
@@ -1,3 +1,7 @@
+-- We are hosting a gift party and need to ensure every guest receives a gift. 
+-- Using the guests and guest_gifts tables, 
+-- write a query to identify the guest(s) who have not been assigned a gift (i.e. they are not listed in the guest_gifts table).
+
 SELECT guest_name 
 FROM guests
 LEFT JOIN guest_gifts
