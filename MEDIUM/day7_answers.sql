f3d6ae661dd43d04bc062cfd273d31befe8e52c1
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 15:56:15 +1100

    Update day7_answers.sql
    included qsn

--- MEDIUM/day7_answers.sql
+++ MEDIUM/day7_answers.sql
@@ -1,3 +1,7 @@
+-- The owner of a winter market wants to know which vendors have generated the highest revenue overall. 
+-- For each vendor, calculate the total revenue for all their items and return a list of the top 2 vendors by total revenue. 
+-- Include the vendor_name and total_revenue in your results.
+
 SELECT 
   vendors.vendor_name, SUM(quantity_sold * price_per_unit) AS total_revenue
 FROM sales 
