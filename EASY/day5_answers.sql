1ea92cd55039496a5e6699b8c1d02059cbf76222
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 15:54:37 +1100

    Update day5_answers.sql
    included qsn

--- EASY/day5_answers.sql
+++ EASY/day5_answers.sql
@@ -1,3 +1,6 @@
+-- This year, we're celebrating Christmas in the Southern Hemisphere! 
+-- Which beaches are expected to have temperatures above 30°C on Christmas Day?
+
 SELECT beach_name
 FROM beach_temperature_predictions
 WHERE date = '2024-12-25'
