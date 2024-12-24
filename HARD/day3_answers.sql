d35b14533bcc2262a2f9a4cfa48541c0eb277493
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 15:52:27 +1100

    Update day3_answers.sql
    included qsn

--- HARD/day3_answers.sql
+++ HARD/day3_answers.sql
@@ -1,3 +1,7 @@
+-- You’re trying to identify the most calorie-packed candies to avoid during your holiday binge. 
+-- Write a query to rank candies based on their calorie count within each category. 
+-- Include the candy_name, candy_category, calories, and rank (rank_in_category) within the category.
+
 SELECT 
   candy_name, 
   candy_category, 
