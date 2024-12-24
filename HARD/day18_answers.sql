612d0613c49ece3920806b00dd88c4f5c04d97ee
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:07:28 +1100

    Update day18_answers.sql
    included qsn

--- HARD/day18_answers.sql
+++ HARD/day18_answers.sql
@@ -1,3 +1,7 @@
+-- A travel agency is promoting activities for a "Summer Christmas" party. 
+-- They want to identify the top 2 activities based on the average rating. 
+-- Write a query to rank the activities by average rating.
+
 SELECT activity_name, AVG(rating) as Avg_rating
 FROM activities a
 JOIN activity_ratings a_r
