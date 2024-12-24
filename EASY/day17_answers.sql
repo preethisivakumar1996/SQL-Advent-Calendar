7ea426d175474877208135fe66604d7387c85c9a
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:06:34 +1100

    Update day17_answers.sql
    included qsn

--- EASY/day17_answers.sql
+++ EASY/day17_answers.sql
@@ -1,3 +1,7 @@
+-- The Grinch is planning out his pranks for this holiday season. 
+-- Which pranks have a difficulty level of “Advanced” or “Expert"? 
+-- List the prank name and location (both in descending order).
+
 SELECT prank_name, location
 FROM grinch_pranks
 WHERE difficulty = "Advanced" OR difficulty = "Expert"
