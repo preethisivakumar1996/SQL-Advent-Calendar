726c556d6e4241093c1cf77bd6329995e47c2a4c
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 15:53:54 +1100

    Update day4_answers.sql
    INCLUDED QSN

--- MEDIUM/day4_answers.sql
+++ MEDIUM/day4_answers.sql
@@ -1,3 +1,7 @@
+-- You’re planning your next ski vacation and want to find the best regions with heavy snowfall. 
+-- Given the tables resorts and snowfall, find the average snowfall for each region and sort the regions in descending order of average snowfall. 
+-- Return the columns region and average_snowfall. 
+
 SELECT ski_resorts.region, AVG(snowfall_inches) AS average_snowfall
 FROM ski_resorts 
 JOIN snowfall 
