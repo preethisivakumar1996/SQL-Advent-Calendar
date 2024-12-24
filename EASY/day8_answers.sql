eafb54e9f8be1715dafbcf63c35302b21c02a064
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 15:56:50 +1100

    Update day8_answers.sql
    included qsn

--- EASY/day8_answers.sql
+++ EASY/day8_answers.sql
@@ -1,3 +1,6 @@
+-- You are managing inventory in Santa's workshop. 
+-- Which gifts are meant for "good" recipients? List the gift name and its weight.
+
 SELECT gift_name, weight_kg
 FROM gifts
 WHERE recipient_type = 'good';
