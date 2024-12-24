b665bfda5e2037f3c82c806a6ec5004ec232d23d
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 16:03:43 +1100

    Update day13_answers.sql
    included qsn

--- MEDIUM/day13_answers.sql
+++ MEDIUM/day13_answers.sql
@@ -1,3 +1,6 @@
+-- We need to make sure Santa's sleigh is properly balanced. 
+-- Find the total weight of gifts for each recipient.
+
 SELECT recipient, SUM(weight_kg) as Total_Weight
 FROM gifts
 GROUP BY recipient;
