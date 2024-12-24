05c5b7333a615adeeea4e0b648d4c33e8f40dfc6
preethisivakumar1996 <yogipreethi1996@gmail.com>
2024-12-24 15:55:30 +1100

    Update day6_answers.sql
    included qsn

--- HARD/day6_answers.sql
+++ HARD/day6_answers.sql
@@ -1,3 +1,7 @@
+-- Scientists are tracking polar bears across the Arctic to monitor their migration patterns and caloric intake. 
+-- Write a query to find the top 3 polar bears that have traveled the longest total distance in December 2024. 
+-- Include their bear_id, bear_name, and total_distance_traveled in the results.
+
 SELECT polar_bears.bear_id, polar_bears.bear_name, SUM(tracking.distance_km) AS total_distance_traveled
 FROM polar_bears 
 JOIN tracking 
